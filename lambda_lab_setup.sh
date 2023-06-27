d226b195712df287e5d5a84817d69b2ef56a55b2
Long <hoanglongdang2001@gmail.com>
2023-06-27 20:46:48 +1000

    add notebook train script for lambda lab cloud setup

---
+++ lambda_lab_setup.sh
@@ -0,0 +1,13 @@
+pip install .
+pip install kaggle pytorch-lightning python-dotenv wandb==0.15.0 protobuf==3.20.3
+
+cd ..
+
+mkdir ./kaggle/input/pokemon-image-dataset
+cd ./kaggle/input/
+kaggle datasets download -d hlrhegemony/pokemon-image-dataset
+unzip pokemon-image-dataset.zip
+rm -f pokemon-image-dataset.zip 
+mv images ./pokemon-image-dataset
+
+cd ../..
