bb3768edd000863387d2a14f40cf2513a12c41a7
Long Dang <hoanglongdang2001@gmail.com>
2025-03-25 19:37:41 +1100

    update pyproject.toml with dependencies so that uv works

--- lambda_lab_setup.sh
+++ lambda_lab_setup.sh
@@ -1,12 +1,12 @@
-pip install kaggle pytorch-lightning python-dotenv wandb==0.15.0 protobuf==3.20.3 boto3
+# install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
 
+# populate dataset
 cd ..
-
 mkdir ./kaggle/input/pokemon-image-dataset
 cd ./kaggle/input/
 kaggle datasets download -d hlrhegemony/pokemon-image-dataset
 unzip pokemon-image-dataset.zip
 rm -f pokemon-image-dataset.zip 
 mv images ./pokemon-image-dataset
-
 cd ../..
